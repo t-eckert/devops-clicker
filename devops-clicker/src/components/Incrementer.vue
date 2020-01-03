3ac754f2d14ac1bf210bae6c7235aac34e83565d
theckert <thomas.james.eckert@gmail.com>
2020-01-02 21:10:15 -0800

    Add Incrementer component

---
+++ devops-clicker/src/components/Incrementer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="score">{{ score }}</div>
+    <button @click="increment()">{{ label }}</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Incrementer",
+
+  props: {
+    target: { type: String, required: true },
+    score: { type: Number, required: true, default: 0 },
+    label: String,
+  },
+
+  methods: {
+    increment() {
+      this.$store.commit(`increment${this.target}`);
+    },
+  },
+});
+</script>
